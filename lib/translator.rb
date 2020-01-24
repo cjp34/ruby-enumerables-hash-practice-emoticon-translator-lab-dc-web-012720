5d00edd699a9adf97feb0655ed52c44b27b41bda
Carl Parm <carlparm@gmail.com>
2020-01-24 17:21:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require "yaml"
 
 def load_library(file_path)
-  YAML.load_file
+  YAML.load_file(file_path)
 end
 
 def get_japanese_emoticon
@@ -11,3 +11,4 @@ end
 def get_english_meaning
   # code goes here
 end
+
