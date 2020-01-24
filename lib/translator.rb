ea975c20b7be181bfa7845675f85350fee94ea97
Carl Parm <carlparm@gmail.com>
2020-01-24 19:04:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,13 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning
-  
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  smiley = library["get_meaning"][emoticon]
+  if smiley
+    smiley
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
 
